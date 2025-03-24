bf31fb2b79a735b283847c8f01177bf41764cd4d
b1shop1234556 <dionecemarkcollano@gmail.com>
2025-03-24 19:02:58 +0800

    testing-02

--- app/Http/Controllers/MenusController.php
+++ app/Http/Controllers/MenusController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Menus;
+use App\Models\Orderitems;
 
 class MenusController extends Controller
 {
--- app/Http/Controllers/OrderitemsController.php
+++ app/Http/Controllers/OrderitemsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Orderitems;
+use App\Models\Menus;
 
 class OrderitemsController extends Controller
 {
